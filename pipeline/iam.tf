c683a77b1cb4550e63eb3b124e0f52356d250380
GerhardFM <gerhard@synthesis.co.za>
2024-08-27 14:19:11 +0200

    Adding sa permissions

--- pipeline/iam.tf
+++ pipeline/iam.tf
@@ -3,111 +3,72 @@ resource "google_service_account" "custom_cloudbuild_sa" {
   display_name = "${var.pipeline_name} Cloud Build Service Account"
 }
 
-resource "google_project_iam_binding" "artifact_registry_reader" {
-  project = var.project_id
-  role    = "roles/artifactregistry.reader"
 
-  members = [
-    "serviceAccount:${google_service_account.custom_cloudbuild_sa.email}",
+resource "google_project_iam_custom_role" "cloudbuild_role" {
+  role_id   = "${var.pipeline_name}-CloudBuildRole"
+  title     = "Custom Cloud Build Role"
+  project   = var.project_id
+  permissions = [
+    "cloudbuild.builds.create",
+    "cloudbuild.builds.update",
+    "cloudbuild.builds.list",
+    "cloudbuild.builds.get",
+    "cloudbuild.workerpools.use",
+    "logging.logEntries.create",
+    "logging.logEntries.list",
+    "logging.views.access",
+    "pubsub.topics.create",
+    "pubsub.topics.publish",
+    "remotebuildexecution.blobs.get",
+    "resourcemanager.projects.get",
+    "resourcemanager.projects.list",
+    "source.repos.get",
+    "source.repos.list",
+    "storage.buckets.create",
+    "storage.buckets.get",
+    "storage.buckets.list",
+    "storage.objects.list",
+    "storage.objects.update",
+    "storage.objects.create",
+    "storage.objects.delete",
+    "storage.objects.get",
+    "artifactregistry.repositories.uploadArtifacts",
+    "artifactregistry.repositories.downloadArtifacts",
+    "artifactregistry.aptartifacts.create",
+    "artifactregistry.dockerimages.get",
+    "artifactregistry.dockerimages.list",
+    "artifactregistry.kfpartifacts.create",
+    "artifactregistry.locations.get",
+    "artifactregistry.locations.list",
+    "artifactregistry.mavenartifacts.get",
+    "artifactregistry.mavenartifacts.list",
+    "artifactregistry.npmpackages.get",
+    "artifactregistry.npmpackages.list",
+    "artifactregistry.projectsettings.get",
+    "artifactregistry.pythonpackages.get",
+    "artifactregistry.pythonpackages.list",
+    "artifactregistry.yumartifacts.create",
+    "artifactregistry.repositories.createOnPush",
+    "artifactregistry.repositories.get",
+    "artifactregistry.repositories.list",
+    "artifactregistry.repositories.listEffectiveTags",
+    "artifactregistry.repositories.listTagBindings",
+    "artifactregistry.tags.create",
+    "artifactregistry.tags.get",
+    "artifactregistry.tags.list",
+    "artifactregistry.tags.update",
+    "artifactregistry.versions.list",
+    "artifactregistry.versions.get",
+    "containeranalysis.occurrences.create",
+    "containeranalysis.occurrences.delete",
+    "containeranalysis.occurrences.get",
+    "containeranalysis.occurrences.list",
+    "containeranalysis.occurrences.update"
   ]
 }
 
-resource "google_project_iam_binding" "pubsub_subscriber" {
+resource "google_project_iam_member" "cloudbuild_role_binding" {
   project = var.project_id
-  role    = "roles/pubsub.subscriber"
-
-  members = [
-    "serviceAccount:${google_service_account.custom_cloudbuild_sa.email}",
-  ]
+  role    = google_project_iam_custom_role.cloudbuild_role.id
+  member  = "serviceAccount:${google_service_account.custom_cloudbuild_sa.email}"
 }
-
-resource "google_project_iam_binding" "iam_service_account_admin" {
-  project = var.project_id
-  role    = "roles/iam.serviceAccountAdmin"
-
-  members = [
-    "serviceAccount:${google_service_account.custom_cloudbuild_sa.email}",
-  ]
-}
-
-resource "google_project_iam_binding" "cloud_run_admin" {
-  project = var.project_id
-  role    = "roles/run.admin"
-
-  members = [
-    "serviceAccount:${google_service_account.custom_cloudbuild_sa.email}",
-  ]
-}
-
-resource "google_project_iam_binding" "cloud_functions_admin" {
-  project = var.project_id
-  role    = "roles/cloudfunctions.admin"
-
-  members = [
-    "serviceAccount:${google_service_account.custom_cloudbuild_sa.email}",
-  ]
-}
-
-resource "google_project_iam_binding" "cloud_sql_admin" {
-  project = var.project_id
-  role    = "roles/cloudsql.admin"
-
-  members = [
-    "serviceAccount:${google_service_account.custom_cloudbuild_sa.email}",
-  ]
-}
-
-resource "google_project_iam_binding" "cloudbuild_permissions" {
-  project = var.project_id
-  role    = "roles/cloudbuild.builds.editor"
-
-  members = [
-    "serviceAccount:${google_service_account.custom_cloudbuild_sa.email}",
-  ]
-}
-
-resource "google_project_iam_binding" "storage_admin" {
-  project = var.project_id
-  role    = "roles/storage.admin"
-
-  members = [
-    "serviceAccount:${google_service_account.custom_cloudbuild_sa.email}",
-  ]
-}
-
-resource "google_project_iam_binding" "compute_admin" {
-  project = var.project_id
-  role    = "roles/compute.admin"
-
-  members = [
-    "serviceAccount:${google_service_account.custom_cloudbuild_sa.email}",
-  ]
-}
-
-resource "google_project_iam_binding" "artifact_registry_admin" {
-  project = var.project_id
-  role    = "roles/artifactregistry.admin"
-
-  members = [
-    "serviceAccount:${google_service_account.custom_cloudbuild_sa.email}",
-  ]
-}
-
-resource "google_project_iam_binding" "iam_service_account_user" {
-  project = var.project_id
-  role    = "roles/iam.serviceAccountUser"
-
-  members = [
-    "serviceAccount:${google_service_account.custom_cloudbuild_sa.email}",
-  ]
-}
-
-resource "google_project_iam_binding" "pubsub_admin" {
-  project = var.project_id
-  role    = "roles/pubsub.admin"
-
-  members = [
-    "serviceAccount:${google_service_account.custom_cloudbuild_sa.email}",
-  ]
-}
-
